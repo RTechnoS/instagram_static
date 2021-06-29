703f4b769696910d31d2195cc767c1cf43154917
RTechnoS <rusmantobyaktasiregar@gmail.com>
2021-06-30 00:23:54 +0700

    perubahan drastis ke oop, dan menambah argsparse

--- main.py
+++ main.py
@@ -1,49 +1,12 @@
 import requests, threading,time
 import datetime
-import argparse
+import argparse,os
 import matplotlib.pyplot as plt
+
 hed = {
 'Host':'www.instagram.com',
 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0'}
-trakhir = 0
-menit_awal = 0
-showBar = 6 #default 6
-updateTime = 60
-hasilAwal = [0,0,0,0,0,0] #ambil 6 data atau 60 menit
-ttt = ['--','--','--','--','--','--']
-
-
-
-def jam():
-	f = datetime.datetime.now().strftime("%H:%M:%S")
-	return f
-
-def chart():
-	def autolabel(rects):
-		for rect in rects:
-			height = rect.get_height()
-
-			if height > 0 :
-				h = '+'+str(height)
-			else:
-				h = height
-
-			plt.annotate('{}'.format(h),
-				xy=(rect.get_x() + rect.get_width() / 2, height),
-				xytext=(0, 2),  # 3 points vertical offset
-				textcoords="offset points",
-				ha='center', va='bottom')	
-	p = plt.bar(ttt[-showBar:], hasilAwal[-showBar:], color='#17eaea')
-	autolabel(p)
-	plt.xticks(rotation = 20)
-	plt.savefig('p.jpg')
-	with open('per10-Minute.txt', 'a') as tls:
-		tls.write(f'{hasilAwal[-1]} ---- {jam()}\n')
-
-	# plt.cla()
-	# plt.clf()
-	plt.close('all')
 
 
 def hitung(jumlah):
@@ -64,59 +27,137 @@ def hitung(jumlah):
 			p = p[3:]
 		jumlah = '.'.join(dt)
 	return jumlah
+def jam():
+	f = datetime.datetime.now().strftime("%H:%M:%S")
+	return f
 
-def st(nama):
-	global trakhir
-	start_sec = 0
 
-	while True:
-		if start_sec == 0:
-			start_sec = time.time()
-		
-		try:
-			js = requests.post(f'http://www.instagram.com/{nama}/channel/?__a=1', headers=hed).json()
-			jml = js['graphql']['user']['edge_followed_by']['count']
-			if trakhir == 0:
-				menit_awal = jml
-				trakhir = jml
+class Mulai:
+	def __init__(self, username, update=1, showbar=6, output=False):
+		self.user = username
+		self.update = 60*update
+		self.showbar = showbar
+		self.trakhir = 0
+		self.menit_awal = 0
+		self.output = output
 
-			if jml < trakhir:
-				y = ('-'+str(trakhir-jml))
-			else:
-				y = ('+'+str(jml-trakhir))
+		self.data = [[[0]*showbar], [['--']*showbar]]
 
-			print(f'({jam()}) ',hitung(str(jml)), f' ({y})')
-			trakhir = jml
+
+
+
+	def st(self):
+		start_sec = 0
+
+		while True:
+			if start_sec == 0:
+				start_sec = time.time()
 			
-		except:
-			pass
+			try:
+				js = requests.post(f'http://www.instagram.com/{self.user}/channel/?__a=1', headers=hed).json()
+				jml = js['graphql']['user']['edge_followed_by']['count']
+				
+				if self.trakhir == 0:
+					self.menit_awal = jml
+					self.trakhir = jml
+
+				if jml < self.trakhir:
+					y = ('-'+str(self.trakhir-jml))
+				else:
+					y = ('+'+str(jml-self.trakhir))
+
+				self.laporan = f'{jam()} | {hitung(str(jml))} ({y})'
+
+				print(self.laporan)
+
+				self.trakhir = jml
+				
+			except:
+				pass
+			
+			if time.time()-start_sec >= self.update: #600 = 10menit
+				#print(datetime.timedelta(seconds=start_sec), datetime.timedelta(seconds=time.time()))
+				#print(self.menit_awal, jml)
+				jum = jml-self.menit_awal
+
+				self.data[0][0].append(jum)
+				self.data[1][0].append(jam())
+				
+				start_sec = time.time()
+				t = threading.Thread(target=self.chart)
+				t.start()
+				self.menit_awal = jml
+			
+			time.sleep(0.8)
+
+
+	def chart(self):
 		
-		if time.time()-start_sec >= updateTime: #600 = 10menit
-			#print(datetime.timedelta(seconds=start_sec), datetime.timedelta(seconds=time.time()))
-			#print(menit_awal, jml)
-			jum = jml-menit_awal
-			ttt.append(jam())
-			hasilAwal.append(jum)
-			start_sec = time.time()
-			t = threading.Thread(target=chart)
-			t.start()
-			menit_awal = jml
+		def autolabel(rects):
+			for rect in rects:
+				height = rect.get_height()
+
+				if height > 0 :
+					h = '+'+str(height)
+				else:
+					h = height
+
+				plt.annotate('{}'.format(h),
+					xy=(rect.get_x() + rect.get_width() / 2, height),
+					xytext=(0, 2),  # 3 points vertical offset
+					textcoords="offset points",
+					ha='center', va='bottom')	
+
+		p = plt.bar(self.data[1][0][-self.showbar:], self.data[0][0][-self.showbar:], color='#17eaea')
+		autolabel(p)
+		plt.xticks(rotation = 20)
+		plt.savefig('bar.jpg')
 		
-		time.sleep(0.8)
+		if output != False:
+			if output == None:
+				n = '@'+self.user+'.txt'
+			else:
+				n = output
+			
+
+			if os.path.exists(n):
+
+				with open(n, 'a') as tls:
+					tls.write(f'{jam()} | {self.menit_awal}  ({self.data[0][0][-1]})\n')
+			else:
+				with open(n, 'w') as tls:
+					tls.write(f'{jam()} | {self.menit_awal}  ({self.data[0][0][-1]})\n')
+
+
+		plt.close('all')
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-u", "--update", help="how many minutes to update char bar. default: 1 minute",type=int)
 	parser.add_argument("-s", "--showbar", help="how many bar in chart. default: 6 bar", type=int)
+	parser.add_argument("-o", "--output", help="save history in a file", nargs='?', const=True) #if isi option -o kosong maka nilai nya adalah True(const=True), tapi tidak berlaku jika -o tidak dipanggil
 	args = parser.parse_args()
+
+	update = 1
+	showbar = 6
+	output = False
+
 	if args.update:
 		if args.update > 0:
-			updateTime = args.update*60
+			update = args.update
+
 	if args.showbar:
 		if args.showbar > 0:
-			hasilAwal = [0]*args.showbar
-			ttt = ['--']*args.showbar
-			showBar = args.showbar
+			showbar = args.showbar
 
+	
+	if args.output:
+		if type(args.output) is not bool:
+			output = str(args.output)
+		else:
+			output = None
+	
 	name = str(input('Username : @'))
-	st(name)
+	p = Mulai(name, update, showbar, output)
+	p = p.st()
